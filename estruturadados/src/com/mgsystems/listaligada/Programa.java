f86efe243ba2ab18c68563a9d016b41eccd0609b
Marcelo de Oliveira <marcelolnoliveira@gmail.com>
2023-10-24 09:14:28 -0300

    Criação e adicao de lista ligada

---
+++ estruturadados/src/com/mgsystems/listaligada/Programa.java
@@ -0,0 +1,20 @@
+package com.mgsystems.listaligada;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		ListaLigada lista= new ListaLigada();
+		lista.adicionar("RJ");
+		lista.adicionar("PE");
+		lista.adicionar("SE");
+		lista.adicionar("AL");
+		lista.adicionar("DF");
+		
+		System.out.println("Primeiro: "+lista.getPrimeiro().getValor());
+		System.out.println("Último: "+lista.getUltimo().getValor());
+		System.out.println("Tamanho: "+lista.getTamanho());
+
+	}
+
+}
